c0a25d826b4b75d1559ca68dad2e490874a15015
Nitheesh627 <nitheeshgowda75@gmail.com>
2026-05-07 08:53:03 +0530

    Sollution

---
+++ GFG/Array/Missing Array.java
@@ -0,0 +1,10 @@
+class Solution {
+    int missingNum(int arr[]) {
+        int diff=0;
+        for(int i=0; i <arr.length ; ++i)
+        {
+            diff+=i+1 - arr[i];
+        }
+        return diff+arr.length+1;
+    }
+}

f18ee3ac60f939c4bdc5ae7dba23fd944731d831
Nitheesh627 <nitheeshgowda75@gmail.com>
2026-05-07 08:59:28 +0530

    Sollution

---
+++ GFG/Strings/Palindrom string.java
@@ -0,0 +1,11 @@
+class Solution {
+    boolean isPalindrome(String s) {
+        int n=s.length();
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
